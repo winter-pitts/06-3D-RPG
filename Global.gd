ea1566770682d08b07150753fbf199b585f35451
winter-pitts <wmpitts@iu.edu>
2022-12-11 03:13:47 -0500

    Updates!

--- Global.gd
+++ Global.gd
@@ -1,5 +1,19 @@
 extends Node
 
-func _unhandled_input(_event):
+var timer = 0
+var score = 0
+
+func _input(_event):
 	if Input.is_action_pressed("menu"):
 		get_tree().quit()
+
+func update_score(s):
+	score += s
+	get_node("/root/Game/UI/Score").text = "Score: " + str(score)
+	
+func update_time():
+	var t_m = floor(timer / 60.0)
+	var t_s = timer % 60
+	var t = "Time: %02d" % t_m
+	t += ":%02d" % t_s
+	get_node("/root/Game/UI/Time").text = t
---
+++ NPCs/NPC.gd
@@ -0,0 +1,27 @@
+extends KinematicBody
+
+onready var Dialogue = get_node("/root/Game/UI/Dialogue")
+
+var dialogue = [
+	"Welcome! There has been an outbreak of of an infection, and we are the only two survivors. "
+	,"Use your gun to shoot any zombies that may appear!"
+	,"Your life depends on it. (Press E to continue)"
+]
+
+func _ready():
+	$AnimationPlayer.play("Idle")
+	Dialogue.connect("finished_dialogue", self, "finished")
+	
+
+func _on_Area_body_entered(body):
+	Dialogue.start_dialogue(dialogue)
+
+
+func _on_Area_body_exited(body):
+	Dialogue.hide_dialogue()
+
+func finished():
+	get_node("/root/Game/Target_container").show()
+	Global.timer = 120
+	Global.update_time()
+	get_node("/root/Game/UI/Timer").start()
